5cffeccc345b6e4be9493364310bfa7c17028a14
Larips(Home) <laripsspiral47@gmail.com>
2025-11-09 18:22:29 +0700

    EffectBuff

--- Assets/_Main/Scripts/Effects/EffectBuff.cs
+++ Assets/_Main/Scripts/Effects/EffectBuff.cs
@@ -1,6 +1,21 @@
-using UnityEngine;
-
-public class EffectBuff
+namespace Main.Effects
 {
-    
+    public class EffectBuff
+    {
+        public float SpeedBuff;
+
+        // Add BuffSpeed
+        public float AddSpeedBuff(float value)
+        {
+
+            return SpeedBuff;
+        }
+
+        // Remove BuffSpeed
+        public float RemoveSpeedBuff(float value)
+        {
+
+            return SpeedBuff;
+        }
+    }
 }
